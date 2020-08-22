65fc147b64f2f63eb9aa87976bc05ee93d5b25f3
Brian Hart <brian.hart@xylogix.net>
2020-08-22 10:34:50 -0400

    Rename porject

--- xyLOGIX.Interop.LibGit2Sharp/Properties/Resources.Designer.cs
+++ xyLOGIX.Interop.LibGit2Sharp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace xyLOGIX.Interop.Git.Properties {
+namespace xyLOGIX.Interop.LibGit2Sharp.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace xyLOGIX.Interop.Git.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("xyLOGIX.Interop.Git.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("xyLOGIX.Interop.LibGit2Sharp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
