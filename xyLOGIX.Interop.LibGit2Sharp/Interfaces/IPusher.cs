42c5783ac7efa211cabac3288fe01be69f65cce3
Brian Hart <brian.hart@xylogix.net>
2020-08-22 11:23:22 -0400

    Updated docs

--- xyLOGIX.Interop.LibGit2Sharp/Interfaces/IPusher.cs
+++ xyLOGIX.Interop.LibGit2Sharp/Interfaces/IPusher.cs
@@ -25,7 +25,7 @@ namespace xyLOGIX.Interop.LibGit2Sharp.Interfaces
         event EventHandler PushStarted;
 
         /// <summary>
-        /// Pushes commits to a remote.
+        /// Pushes commits from the master branch to a remote called origin.
         /// </summary>
         /// <exception
         ///     cref="T:xyLOGIX.Interop.Git.Exceptions.RepositoryNotAttachedException">
@@ -33,6 +33,24 @@ namespace xyLOGIX.Interop.LibGit2Sharp.Interfaces
         /// <see
         ///     cref="M:xyLOGIX.Interop.Git.Interfaces.IRepositoryBoundObject.AttachRepository" />
         /// method has not been called.
+        /// </exception>
+        /// <exception
+        ///     cref="T:xyLOGIX.Interop.LibGit2Sharp.Exceptions.RepositoryNotConfiguredException">
+        /// Thrown
+        /// if either the
+        /// <see
+        ///     cref="P:xyLOGIX.Interop.LibGit2Sharp.Internal.RepositoryBoundObject.GitHubName" />
+        /// ,
+        /// <see
+        ///     cref="P:xyLOGIX.Interop.LibGit2Sharp.Internal.RepositoryBoundObject.GitHubEmail" />
+        /// ,
+        /// <see
+        ///     cref="P:xyLOGIX.Interop.LibGit2Sharp.Internal.RepositoryBoundObject.GitHubUserName" />
+        /// , or
+        /// <see
+        ///     cref="P:xyLOGIX.Interop.LibGit2Sharp.Internal.RepositoryBoundObject.GitHubPassword" />
+        /// are blank.
+        /// properties are blank.
         /// </exception>
         void Push();
     }
