c639b662a8389256df0d9ac189fd007908113dca
Brian Hart <brian.hart@xylogix.net>
2020-08-24 10:25:51 -0400

    Create TeamConfigurationException class

--- xyLOGIX.Interop.LibGit2Sharp/Exceptions/TeamConfigurationException.cs
+++ xyLOGIX.Interop.LibGit2Sharp/Exceptions/TeamConfigurationException.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using xyLOGIX.Interop.LibGit2Sharp.Interfaces;
 
 namespace xyLOGIX.Interop.LibGit2Sharp.Exceptions
 {
@@ -14,7 +15,33 @@ namespace xyLOGIX.Interop.LibGit2Sharp.Exceptions
         ///     cref="T:xyLOGIX.Interop.LibGit2Sharp.Exceptions.TeamConfigurationException" />
         /// and returns a reference to it.
         /// </summary>
-        public TeamConfigurationException(string message) : base(
-            $"The Team is not configured properly.\n\n{message}") { }
+        /// <param name="team">
+        /// (Required.) Reference to an instance of an object that
+        /// implements the <see cref="T:xyLOGIX.Interop.LibGit2Sharp.Interfaces.ITeam" />
+        /// interface. This object represents the Team that this exception is talking
+        /// about.
+        /// </param>
+        /// <param name="message">
+        /// (Optional.) Message, if any, to be added to this
+        /// exception's default message.
+        /// </param>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// Thrown if the
+        /// <paramref name="team" /> parameter is passed a null reference.
+        /// </exception>
+        public TeamConfigurationException(ITeam team, string message = "") :
+            base(!string.IsNullOrWhiteSpace(message)
+                ? $"The Team is not configured properly.\n\n{message}"
+                : "The Team is not configured properly.")
+        {
+            Team = team ?? throw new ArgumentNullException(nameof(team));
+        }
+
+        /// <summary>
+        /// Gets a reference to an instance of an object that implements the
+        /// <see cref="T:xyLOGIX.Interop.LibGit2Sharp.Interfaces.ITeam" /> interface that
+        /// represents the Team this exception is talking about.
+        /// </summary>
+        public ITeam Team { get; }
     }
 }
