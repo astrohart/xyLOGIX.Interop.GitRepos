035bcff633396c73070890296ac0ea7cd2b26f56
Brian Hart <brian.hart@xylogix.net>
2020-08-24 10:07:35 -0400

    Create TeamConfigurationException object.

---
+++ xyLOGIX.Interop.LibGit2Sharp/Exceptions/TeamConfigurationException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace xyLOGIX.Interop.LibGit2Sharp.Exceptions
+{
+    /// <summary>
+    /// Exception object to be thrown in the event that a Team object is not properly
+    /// configured.
+    /// </summary>
+    public class TeamConfigurationException : InvalidOperationException
+    {
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see
+        ///     cref="T:xyLOGIX.Interop.LibGit2Sharp.Exceptions.TeamConfigurationException" />
+        /// and returns a reference to it.
+        /// </summary>
+        public TeamConfigurationException(string message) : base(
+            $"The Team is not configured properly.\n\n{message}") { }
+    }
+}
